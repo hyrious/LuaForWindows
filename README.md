83aeb5cbf6364d8a44e7ae01da26ff17cc5d49bc
hyrious <hyrious@outlook.com>
2017-11-23 23:57:46 +0800

    remove zip step

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Require
 
-- msys2 ( buildtools, zip, curl )
+- msys2 ( buildtools, curl )
 
 ## Make
 
@@ -17,15 +17,24 @@ Building lua-5.3.4 ..
 Check version:
 src/lua -v
 Lua 5.3.4  Copyright (C) 1994-2017 Lua.org, PUC-Rio
-Making target [lua-5.3.4\install] ..
-Ok.
-Zipping lua.zip ..
+Making target [lua-5.3.4\install -> lua] ..
 Ok.
 clean
 Ok.
 ```
 
-Then there will be `lua.zip` in this folder.
+Then there will be `lua\` in this folder, which contains:
+
+```
+bin:
+    lua luac 
+include:
+    lua.h luaconf.h lualib.h lauxlib.h lua.hpp 
+lib:
+    liblua.a 
+man/man1:
+    lua.1 luac.1 
+```
 
 ## License
 
--- build.cmd
+++ build.cmd
@@ -5,6 +5,7 @@ set VERSION=5.3.4
 set FILENAME=lua-%VERSION%.tar.gz
 set DIRNAME=lua-%VERSION%
 
+if exist lua call :Clean
 if not exist %FILENAME% call :Download
 echo Unzipping %FILENAME% ..
 >nul tar zxvf %FILENAME%
@@ -14,14 +15,10 @@ echo Building %DIRNAME% ..
 >nul make mingw
 echo Check version:
 make test
-echo Making target [%DIRNAME%\install] ..
+echo Making target [%DIRNAME%\install -^> lua] ..
 >nul make local
+>nul move /y install ..\lua
 echo Ok.
-pushd install
-echo Zipping lua.zip ..
-zip -q ../../lua.zip ./*
-echo Ok.
-popd
 popd
 echo clean
 >nul del /s/f/q %DIRNAME%
@@ -33,5 +30,12 @@ goto :EOF
 :Download
 echo Downloading %FILENAME% ..
 start /w curl -R -O https://www.lua.org/ftp/%FILENAME%
+echo Ok.
+goto :EOF
+
+:Clean
+echo Cleaning previous files ..
+>nul del /s/f/q lua
+rd /s/q lua
 echo Ok.
 goto :EOF
